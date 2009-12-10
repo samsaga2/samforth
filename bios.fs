7abe058287b76abbdb33682fbde7033f1b48744b
victor <victor@mi-ordenadorcito.(none)>
2009-12-10 14:41:38 +0100

    Variables de la bios

--- bios.fs
+++ bios.fs
@@ -1,11 +1,81 @@
 hex
 
 \ system vars
-f3ae const linl40
-f3af const linl32
-f3e9 const forclr
-f3ea const bakclr
-f3eb const bdrclr
+F380 const SYS-RDPRIM	\ Routine that reads from a primary slot
+F385 const SYS-WRPRIM	\ Routine that writes to a primary slot
+F38C const SYS-CLPRIM	\ Routine that calls a routine in a primary slot
+F39A const SYS-USRTAB	\ Address to call with Basic USR0
+F3AE const SYS-LINL40	\ Width for SCREEN 0 (default 37)
+F3AF const SYS-LINL32	\ Width for SCREEN 1 (default 29)
+F3B0 const SYS-LINLEN	\ Width for the current text mode
+F3B1 const SYS-CRTCNT	\ Number of lines on screen
+F3B2 const SYS-CLMLST	\ Column space. It’s uncertain what this address actually stores
+F3B3 const SYS-TXTNAM	\ BASE(0) - SCREEN 0 name table
+F3B5 const SYS-TXTCOL	\ BASE(1) - SCREEN 0 color table
+F3B7 const SYS-TXTCGP	\ BASE(2) - SCREEN 0 character pattern table
+F3B9 const SYS-TXTATR	\ BASE(3) - SCREEN 0 Sprite Attribute Table
+F3BB const SYS-TXTPAT	\ BASE(4) - SCREEN 0 Sprite Pattern Table
+F3B3 const SYS-T32NAM	\ BASE(5) - SCREEN 1 name table
+F3B5 const SYS-T32COL	\ BASE(6) - SCREEN 1 color table
+F3B7 const SYS-T32CGP	\ BASE(7) - SCREEN 1 character pattern table
+F3B9 const SYS-T32ATR	\ BASE(8) - SCREEN 1 sprite attribute table
+F3BB const SYS-T32PAT	\ BASE(9) - SCREEN 1 sprite pattern table
+F3B3 const SYS-GRPNAM	\ BASE(10) - SCREEN 2 name table
+F3B5 const SYS-GRPCOL	\ BASE(11) - SCREEN 2 color table
+F3B7 const SYS-GRPCGP	\ BASE(12) - SCREEN 2 character pattern table
+F3B9 const SYS-GRPATR	\ BASE(13) - SCREEN 2 sprite attribute table
+F3BB const SYS-GRPPAT	\ BASE(14) - SCREEN 2 sprite pattern table
+F3B3 const SYS-MLTNAM	\ BASE(15) - SCREEN 3 name table
+F3B5 const SYS-MLTCOL	\ BASE(16) - SCREEN 3 color table
+F3B7 const SYS-MLTCGP	\ BASE(17) - SCREEN 3 character pattern table
+F3B9 const SYS-MLTATR	\ BASE(18) - SCREEN 3 sprite attribute table
+F3BB const SYS-MLTPAT	\ BASE(19) - SCREEN 3 sprite pattern table
+F3DB const SYS-CLIKSW	\ =0 when key press click disabled =1 when key press click enabled
+F3DC const SYS-CSRY	\ Current row-position of the cursor
+F3DD const SYS-CSRX	\ Current column-position of the cursor
+F3DE const SYS-CNSDFG	\ =0 when function keys are not displayed =1 when function keys are displayed
+F3DF const SYS-RG0SAV	\ Content of VDP(0) register (R#0)
+F3E0 const SYS-RG1SAV	\ Content of VDP(1) register (R#1)
+F3E1 const SYS-RG2SAV	\ Content of VDP(2) register (R#2)
+F3E2 const SYS-RG3SAV	\ Content of VDP(3) register (R#3)
+F3E3 const SYS-RG4SAV	\ Content of VDP(4) register (R#4)
+F3E4 const SYS-RG5SAV	\ Content of VDP(5) register (R#5)
+F3E5 const SYS-RG6SAV	\ Content of VDP(6) register (R#6)
+F3E6 const SYS-RG7SAV	\ Content of VDP(7) register (R#7)
+F3E7 const SYS-STATFL	\ Content of VDP(8) status register (S#0)
+F3E8 const SYS-TRGFLG	\ Information about trigger buttons and space bar state
+F3E9 const SYS-FORCLR	\ Foreground color
+F3EA const SYS-BAKCLR	\ Background color
+F3EB const SYS-BDRCLR	\ Border color
+F3EC const SYS-MAXUPD	\ Jump instruction used by Basic LINE command. The routines used are: RIGHTC, LEFTC, UPC and DOWNC
+F3EF const SYS-MINUPD	\ Jump instruction used by Basic LINE command. The routines used are: RIGHTC, LEFTC, UPC and DOWNC
+F3F2 const SYS-ATRBYT	\ Attribute byte (for graphical routines it’s used to read the color)
+F3F3 const SYS-QUEUES	\ Address of the queue table
+F3F5 const SYS-FRCNEW	\ CLOAD flag =0 when CLOAD =255 when CLOAD?
+F3F6 const SYS-SCNCNT	\ Key scan timing
+F3F7 const SYS-REPCNT	\ This is the key repeat delay counter
+F3F8 const SYS-PUTPNT	\ Address in the keyboard buffer where a character will be written
+F3FA const SYS-GETPNT	\ Address in the keyboard buffer where the next character is read
+F3FC const SYS-CS120	\ Cassette I/O parameters to use for 1200 baud
+F401 const SYS-CS240	\ Cassette I/O parameters to use for 2400 baud
+F406 const SYS-LOW	\ Signal delay when writing a 0 to tape
+F408 const SYS-HIGH	\ Signal delay when writing a 1 to tape
+F40A const SYS-HEADER	\ Delay of tape header (sync.) block
+F40B const SYS-ASPCT1	\ Horizontal / Vertical aspect for CIRCLE command
+F40D const SYS-ASPCT2	\ Horizontal / Vertical aspect for CIRCLE command
+F40F const SYS-ENDPRG	\ Pointer for the RESUME NEXT command
+F414 const SYS-ERRFLG	\ Basic Error code
+F415 const SYS-LPTPOS	\ Position of the printer head
+F416 const SYS-PRTFLG	\ Printer output flag is read by OUTDO
+F417 const SYS-NTMSXP	\ Printer type is read by OUTDO. SCREEN ,,,n writes to this address
+F418 const SYS-RAWPRT	\ Raw printer output is read by OUTDO
+F419 const SYS-VLZADR	\ Address of data that is temporarilly replaced by ‘O’ when Basic function VAL("") is running
+F41B const SYS-VLZDAT	\ Original value that was in the address pointed to with VLZADR
+F41C const SYS-CURLIN	\ Line number the Basic interpreter is working on, in direct mode it will be filled with #FFFF
+FAF8 const SYS-EXBRSA	\ Slot address of the SUBROM (EXtended Bios-Rom Slot Address)
+FB21 const SYS-DRVINF	\ Nr. of drives connected to disk interface 1
+FCC1 const SYS-EXPTBL	\ Slot 0: #80 = expanded, 0 = not expanded. Also slot address of the main BIOS-ROM.
+FCC5 const SYS-SLTTBL	\ Mirror of slot 0 secondary slot selection register.
 
 : disable-screen ( -- )
     0 0 0 0 0041 SYSCALL 4drop ;
@@ -40,7 +110,7 @@ f3eb const bdrclr
     0 0 0 r> 00c6 SYSCALL 4drop ;
 
 : change-color ( bordercolor backgroundcolor foregroundcolor -- )
-    forclr c! bakclr c! bdrclr c!
+    sys-forclr c! sys-bakclr c! sys-bdrclr c!
     0 0 0 0 0062 SYSCALL 4drop ;
 
 : init-text ( -- )
--- test.fs
+++ test.fs
@@ -2,7 +2,7 @@ INCLUDE kernel.fs
 INCLUDE bios.fs
 
 binary
-create test-char 
+create star-pattern 
 00000000 c,
 00000000 c,
 00000000 c,
@@ -14,35 +14,32 @@ create test-char
 00000000 c,
 decimal
 
-: char-addr ( c -- v-addr )
+: char-vaddr ( c -- v-addr )
     3 lshift ;
 
-: bold-byte ( i -- j )
-    dup 1 rshift or ;
-
 : bold-font ( -- )
-    127 char-addr 7 +
-    32 char-addr
+    127 char-vaddr 7 +
+    32 char-vaddr
     do
         i vram@
-        bold-byte i vram!
+        dup 1 rshift or i vram!
     loop ;
 
 : redefine-char ( char addr -- )
     >r 8 swap 3 lshift r> ram-to-vram ;
 
 : show-message
-    ." ************************** " cr
+    ." **************************" cr
     ." SamForth " 2009 decimal . cr
     ." by Victor Marzo" cr
     ." <samsaga2@gmail.com>" cr
-    ." ************************** " cr cr ;
+    ." **************************" cr cr ;
 
 : init-screen
-    32 linl32 c!
+    32 sys-linl32 c!
     init-mode32 0 0 15 change-color
     bold-font
-    [char] * test-char redefine-char ;
+    [char] * star-pattern redefine-char ;
 
 : main
     init-screen show-message abort ;
